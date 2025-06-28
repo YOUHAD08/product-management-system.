5e6f130257f87f1d100baaa97c8e3cd8f88f8387
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-06-29 00:27:10 +0100

    Added Angular service to fetch and delete products via HttpClient, and configured app to use provideHttpClient for HTTP support

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -18,12 +18,18 @@ export class Products implements OnInit {
     this.getProducts()
   }
   getProducts(): void {
-    this.products= this.Product.getAllProducts()
+    this.products= this.Product.getAllProducts().subscribe({
+      next: response => { this.products = response;},
+      error: error => console.log(error)
+    })
   }
   deleteProduct(product: any):  void{
     let Confirmed = confirm("Are you sure you want to delete this product?");
     if (Confirmed) {
-      this.Product.deleteProduct(product);
+      this.Product.deleteProduct(product).subscribe({
+        next: response => { this.Product.getAllProducts();},
+        error: error => console.log(error)
+      });
       this.getProducts();
     }
   }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,24 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
   // This makes sure that this service is available to all components.
 })
 export class Product {
+  constructor(private http: HttpClient) {}
 
-  products = [
-    {id:1, name:"Computer", price:2300, selected:true},
-    {id:2, name:"Printer", price:1200 , selected:false},
-    {id:3, name:"Smart Phone", price: 1100 , selected:true}
-  ];
-  constructor() { }
-
-  getAllProducts():  any {
-    return this.products;
+  getAllProducts():  Observable<any> {
+    return this.http.get("http://localhost:8080/products")
   }
 
-  deleteProduct(product: any):  void{
-      this.products.splice(this.products.indexOf(product),1);
+  deleteProduct(product: any){
+      return this.http.delete(`http://localhost:8080/products/${product.id}`)
       // this.products.filter((p: { id: any; })=> p.id !== product.id);
 
   }
